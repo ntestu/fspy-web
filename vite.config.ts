e4761d90f452acaedcfc104a35588d26bb9fd2c7
ntestu <189612740+ntestu@users.noreply.github.com>
2025-08-31 17:46:05 +0200

    Improve chunking

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          konva: ['react-konva'],
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
   plugins: [react()],
   publicDir: 'assets/electron',
   server: {
